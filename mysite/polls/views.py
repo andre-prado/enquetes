4b526d828945c64b952d2c07193da9d50d5a1909
andre-prado <andr3prad0@gmail.com>
2020-12-19 16:07:31 -0300

    adciona templates html com jinja >> templates/polls/index.html & detail.html

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,12 +1,19 @@
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+
+from .models import Question
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
 
 def detail(request, quest_id):
-    return HttpResponse(f"You're looking at question {quest_id}")
+    question = get_object_or_404(Question, pk=quest_id)
+    context = {"question": question}
+    return render(request, "polls/detail.html", context)
 
 
 def results(request, quest_id):
