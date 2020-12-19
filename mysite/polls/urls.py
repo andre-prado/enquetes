c884aa8ae198c63ccdf8a413d70ab52c26c77b2e
andre-prado <andr3prad0@gmail.com>
2020-12-19 14:52:59 -0300

    cria views > detail, results, vote e as registra em urls.py

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -4,6 +4,12 @@ from . import views
 
 
 urlpatterns = [
-  # path("teste", views.caminho_teste, name="caminho_teste"),
-  path("", views.index, name="index")
+  # ex: /polls/
+  path("", views.index, name="index"),
+  # ex: /polls/5/
+  path("<int:quest_id>/", views.detail, name="details"),
+  # ex: /polls/5/results/
+  path("<int:quest_id>/results/", views.results, name="results"),
+  # ex: /polls/5/vote/
+  path("<int:quest_id>/vote/", views.vote, name="vote")
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -4,5 +4,15 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
-# def caminho_teste(request):
-#     return HttpResponse("Olá, você está na página de teste.")
+
+def detail(request, quest_id):
+    return HttpResponse(f"You're looking at question {quest_id}")
+
+
+def results(request, quest_id):
+    response = f"You're looking at the results of question {quest_id}"
+    return HttpResponse(response)
+
+
+def vote(request, quest_id):
+    return HttpResponse(f"You're voting on question {quest_id}")
